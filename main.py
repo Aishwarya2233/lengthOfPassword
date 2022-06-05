1c97403c7cefc64f894fef57e9d9e3d33d083540
Ishushetty <aishwaryakolipaka2233@gmail.com>
2022-06-05 15:55:24 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+lengthOfPassword = int(input("enter the length of password: "))
+s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+p = "".join(random.sample(s,lengthOfPassword ))
+print(p)
